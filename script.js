dde4a5e3a1463da5e234232396f0712c5303d3e4
ALINE  BRANDÃO <143004609+alin332r@users.noreply.github.com>
2025-01-03 22:09:29 -0300

    Update script.js
    update

--- script.js
+++ script.js
@@ -34,29 +34,3 @@ setInterval(showRandomQuote, 10000);  // 5000 milissegundos = 5 segundos
 // Adiciona evento para o botão de nova citação
 document.getElementById('new-quote').addEventListener('click', showRandomQuote);
 
-=======
-const nav1 = document.querySelector('nav a:first-child');
-const nav2 = document.querySelector('nav a:nth-child(2)');
-const page1 = document.querySelector('.page1');
-const page2 = document.querySelector('.page2');
-
-nav1.addEventListener('click', (event) => {
-  event.preventDefault();
-  document.startViewTransition(() => {
-    page2.style.display = 'none';
-    page1.style.display = 'block';
-    nav2.classList.remove('active');
-    nav1.classList.add('active');
-  });
-})
-
-nav2.addEventListener('click', (event) => {
-  event.preventDefault();
-  document.startViewTransition(() => {
-    page1.style.display = 'none';
-    page2.style.display = 'block';
-    nav1.classList.remove('active');
-    nav2.classList.add('active');        
-  });      
-})    
->>>>>>> 4b087b36fa30277ee1ec37ebaf91ae574d5692f8
